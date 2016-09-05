3fa91ca4b754f7b8f3ddd8d96ca3c597a28e733e
임시환-PC\임시환 <tlghks1009@naver.com>
2016-09-05 23:19:34 +0900

    no message

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    float h;
+    float v;
+
+    bool Moves;
+
+    Vector2 startPos;
+    Vector2 currentPos;
+
+    int touchCount;
+    void Start()
+    {
+        GameManager.instance.GameLoopingSet(GameSetting.GameStart);
+        Moves = false;
+        startPos = transform.position;
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //hori 가로 veri 세로.
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            Moves = true;
+
+        if (Moves)
+        {
+            PlayerMove(h, v);
+        }
+        
 	}
+    void PlayerMove(float h, float v)
+    {
+        if (h == -1) //가로
+        {
+            transform.DOMoveX(transform.position.x - 1, 0);
+            Moves = false;
+        }
+        if (h == 1) //가로
+        {
+            transform.DOMoveX(transform.position.x + 1, 0);
+            Moves = false;
+        }
+        if(v == 1)
+        {
+            transform.DOMoveY(transform.position.y + 1, 0);
+            Moves = false;
+        }
+        if(v == -1)
+        {
+            transform.DOMoveY(transform.position.y - 1, 0);
+            Moves = false;
+        }
+    }
 }
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum GameSetting
+{
+    GameInit,
+    GameStart,
+    GamePause,
+    GamePlaying,
+    GameSet
+}
+
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameManager();
+            }
+            return _instance;
+        }
+    }
+    
+    const int StartPosX = 0;
+    const int StartPosY = 0;
+
+    public GameObject MainCam;
+    public GameObject Background;
+    public GameObject tile;
+    public GameObject Player;
+
+    public GameSetting gameSetting;
+
+	void Start ()
+    {
+        if(gameSetting == GameSetting.GameInit)
+            init();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void init()
+    {
+        GameObject goTemp = Instantiate(Background) as GameObject;
+        GameObject goTemp_tile = Instantiate(tile) as GameObject;
+        goTemp.transform.parent = MainCam.gameObject.transform;
+        goTemp_tile.transform.parent = goTemp.transform;
+        Player = Instantiate(Player, new Vector2(StartPosX, StartPosY), Quaternion.identity) as GameObject;
+        Player.AddComponent<GameController>();
+    }
+    public void GameLoopingSet(GameSetting gameSetting)
+    {
+        switch (gameSetting)
+        {
+            case GameSetting.GameInit:
+                init();
+                break;
+            case GameSetting.GameStart:
+                gameStart();
+                break;
+            case GameSetting.GamePlaying:
+                break;
+            case GameSetting.GameSet:
+                break;
+            case GameSetting.GamePause:
+                break;
+        }
+    }
+    void gameStart()
+    {
+        Debug.Log("=====게임 시작=======");
+    }
 }
